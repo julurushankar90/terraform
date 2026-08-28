8a07d5056ac6659068fc12693f71887c8c5ff364
julurushankar90 <julurushankar90@gmail.com>
2026-08-28 18:06:18 +0530

    terraform

--- count/r53.tf
+++ count/r53.tf
@@ -7,3 +7,14 @@ resource "aws_route53_record" "www" {
   ttl     = 1
   records = [aws_instance.example[count.index].private_ip]
 }
+
+# roboshop.julurushankar.online -> public_ip
+# as part of functions
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  # interpolation
+  name    = "roboshop.${var.domain_name}" # mongodb.julurushankar.online
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.example[index(var.instances, "frontend")].public_ip]
+}
--- for_each/r53.tf
+++ for_each/r53.tf
@@ -8,3 +8,12 @@ resource "aws_route53_record" "www" {
   records = [each.value.private_ip]
   allow_overwrite = true
 }
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  # interpolation
+  name    = "roboshop.${var.domain_name}" # mongodb.julurushankar.online
+  type    = "A"
+  ttl     = 1
+  records = [lookup(aws_instance.example, "frontend").public_ip]
+  allow_overwrite = true
+}
